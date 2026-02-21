512ab0ad08c189ece04c24c2ef814fd5ab9bebee
SalmaYasserMoselhi <salmayasser816@gmail.com>
2026-02-21 16:00:13 +0200

    feat: add pagination utility and logger middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const cors = require('cors');
 const express = require('express');
 const {getUploadSignature} = require('./src/controllers/upload');
+const {httpLogger} = require('./src/middlewares/logger');
 const bookRoutes = require('./src/routes/book.routes');
 const cartRoutes = require('./src/routes/cart');
 const categoryRoutes = require('./src/routes/category.routes');
@@ -8,6 +9,7 @@ const categoryRoutes = require('./src/routes/category.routes');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(httpLogger);
 
 app.use('/api/categories', categoryRoutes);
 app.use('/api/books', bookRoutes);

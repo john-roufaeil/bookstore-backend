b04b8bbf63a32a5b058146f381814e774ceb8c51
john <john.flouis1@gmail.com>
2026-02-21 19:46:24 +0200

    fix: app.js

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const cors = require('cors');
 const express = require('express');
-const {getUploadSignature} = require('./src/controllers/upload');
-const {httpLogger} = require('./src/middlewares/logger');
-const bookRoutes = require('./src/routes/book.routes');
-const cartRoutes = require('./src/routes/cart');
-const categoryRoutes = require('./src/routes/category.routes');
+const { httpLogger } = require('./src/middlewares/logger');
+const routes = require('./src/routes');
 
 const app = express();
 app.use(cors());

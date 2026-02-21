58ae2cc71720e962c606d6003a3210710fe308d4
john <john.flouis1@gmail.com>
2026-02-21 15:52:42 +0200

    refactor: use index routes in app

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const { getUploadSignature } = require('../controllers/cloudinary');
 
 const router = express.Router();
 
+router.get('/cloudinary-signature', getUploadSignature);
 router.use('/cart', require('./cart'));
+router.use('/authors', require('./author'));
+router.use('/reviews', require('./review'));
+router.use('/categories', require('./category.routes'));
+router.use('/books', require('./book.routes'));
 
 module.exports = router;

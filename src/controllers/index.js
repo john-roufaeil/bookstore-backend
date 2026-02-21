718f793c174750e43d337bb56f51a9692b8f4894
RanaMohamed24 <raanaamohamed695@gmail.com>
2026-02-21 15:47:07 +0200

    add book and category exports to  index files

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,5 @@
 const cart = require('./cart');
+const book = require('./book.controller');
+const category = require('./category.controller');
 
-module.exports = {cart};
+module.exports = { cart, book, category };

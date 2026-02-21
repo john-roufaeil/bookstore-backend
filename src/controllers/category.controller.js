5c4eb1695fd7c8589ea0dc140b99d6ad4545496a
RanaMohamed24 <raanaamohamed695@gmail.com>
2026-02-21 15:46:12 +0200

    use ApiError and ApiResponse, remove inline validation

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,19 +1,19 @@
 const Book = require('../models/book.model');
 const Category = require('../models/category.model');
-const { ApiResponse, ApiError } = require('../utils');
+const {ApiResponse, ApiError} = require('../utils');
 
 const getAllCategories = async (req, res) => {
-  const categories = await Category.find().sort({ name: 1 });
+  const categories = await Category.find().sort({name: 1});
   res.json(new ApiResponse(200, 'Categories fetched', categories));
 };
 
 const createCategory = async (req, res) => {
-  const category = await Category.create({ name: req.body.name });
+  const category = await Category.create({name: req.body.name});
   res.status(201).json(new ApiResponse(201, 'Category created', category));
 };
 
 const updateCategory = async (req, res) => {
-  const category = await Category.findByIdAndUpdate(req.params.id, { name: req.body.name }, { returnDocument: 'after' });
+  const category = await Category.findByIdAndUpdate(req.params.id, {name: req.body.name}, {returnDocument: 'after'});
   if (!category) throw new ApiError(404, 'Category not found');
 
   res.json(new ApiResponse(200, 'Category updated', category));
@@ -23,7 +23,7 @@ const deleteCategory = async (req, res) => {
   const category = await Category.findById(req.params.id);
   if (!category) throw new ApiError(404, 'Category not found');
 
-  await Book.updateMany({ category: req.params.id }, { category: null });
+  await Book.updateMany({category: req.params.id}, {category: null});
   await Category.findByIdAndDelete(req.params.id);
 
   res.json(new ApiResponse(200, 'Category deleted, Books in this Category are now uncategorized'));

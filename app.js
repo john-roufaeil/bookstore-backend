970a3b80e2a54a67b1e4d436c2775c324ae38819
SalmaYasserMoselhi <salmayasser816@gmail.com>
2026-02-21 16:00:13 +0200

    feat: add pagination utility and logger middleware

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const cors = require('cors');
 const express = require('express');
+const { getUploadSignature } = require('./src/controllers/upload');
+const { httpLogger } = require('./src/middlewares/logger');
 const routes = require('./src/routes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(httpLogger);
+
 app.use('/api', routes);
+app.get('/api/upload/signature', getUploadSignature);
 
 module.exports = app;
